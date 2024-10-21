1d48cadcabd6c310155c04e1b98fe560e447d124
Abdullahsabir123 <abdullahsabir558@gmail.com>
2024-10-21 10:42:43 +0500

    completed

--- src/Components/Welcome.jsx
+++ src/Components/Welcome.jsx
@@ -29,8 +29,8 @@ useEffect(() => {
         <center>
           <ColorRing
             visible={loader}
-            height="200"
-            width="200"
+            height="150"
+            width="150"
             ariaLabel="color-ring-loading"
             wrapperStyle={{}}
             wrapperClass="color-ring-wrapper"
@@ -44,40 +44,3 @@ useEffect(() => {
     </div>
   );
 }
-
-// import React, { useState, useEffect } from "react";
-// import "./welcome.css";
-// import { ColorRing } from "react-loader-spinner";
-
-// export default function Welcome() {
-//   const [loader, setLoader] = useState(true);
-
-//   useEffect(() => {
-//     const load = setTimeout(() => {
-//       setLoader(false);
-//     }, 5000);
-
-//     return () => clearTimeout(load);
-//   }, []);
-
-//   return (
-//     <div>
-//       <div>
-//         <center>
-//           <ColorRing
-//             visible={loader}
-//             height="80"
-//             width="80"
-//             ariaLabel="color-ring-loading"
-//             wrapperStyle={{}}
-//             wrapperClass="color-ring-wrapper"
-//             colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
-//           />
-//         </center>
-//       </div>
-//       <div className="img1">
-//         <h1 className="text-light pos">Welcome to my website</h1>
-//       </div>
-//     </div>
-//   );
-// }
